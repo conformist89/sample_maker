a80c5ef834749d822bacc6fb7715d19a5b3e903f
Olha Lavoryk <olhalavoryk@gmail.com>
2024-07-16 11:16:42 +0200

    Added config and modified script to retrieve information from few html files

--- webparser.py
+++ webparser.py
@@ -1,27 +1,34 @@
 from bs4 import BeautifulSoup
+import json
 
-# Read the HTML file
-with open('/home/olavoryk/Downloads/Skimmed datasets.html', 'r', encoding='windows-1252') as file:
-    html_content = file.read()
+f = open('config.json')
+data = json.load(f)
 
-# Parse the HTML content
-soup = BeautifulSoup(html_content, 'html.parser')
+html_files = []
 
-# Initialize lists to store data
-dataset_info = []
+for i in data["2018"]:
+    html_files.append(i)
 
-# Extract data from each row
-rows = soup.select('.jsgrid-table tbody tr.jsgrid-row, .jsgrid-table tbody tr.jsgrid-alt-row')
-for row in rows:
-    columns = row.find_all('td')
-    dataset_name = columns[1].text.strip()
-    num_files = int(columns[3].text.strip())
-    num_selected_events = int(columns[4].text.strip())
-    dataset_info.append((dataset_name, num_files, num_selected_events))
 
-# Print dataset information
-for name, files, events in dataset_info:
-    print(f"Dataset Name: {name}")
-    print(f"Number of Files: {files}")
-    print(f"Number of Selected Events: {events}")
-    print("=" * 20)
+for fullpath in html_files:
+    
+    with open(fullpath) as html_file:
+        soup = BeautifulSoup(html_file, 'lxml')
+
+        dataset_info = []
+
+        # Extract data from each row
+        rows = soup.select('.jsgrid-table tbody tr.jsgrid-row, .jsgrid-table tbody tr.jsgrid-alt-row')
+        for row in rows:
+            columns = row.find_all('td')
+            dataset_name = columns[1].text.strip()
+            num_files = int(columns[3].text.strip())
+            num_selected_events = int(columns[4].text.strip())
+            dataset_info.append((dataset_name, num_files, num_selected_events))
+
+        # Print dataset information
+        for name, files, events in dataset_info:
+            print(f"Dataset Name: {name}")
+            print(f"Number of Files: {files}")
+            print(f"Number of Selected Events: {events}")
+            print("=" * 20)
